65135bdb86d7ef688845c40b8e643cd95b4f323d
hsl947 <1506070803@qq.com>
2020-02-09 17:34:16 +0800

    路由配置优化

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,26 @@
 import React from 'react'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 
-import Home from '@/pages/test'
+import pages from './pages'
 
 const router = () => {
 	return (
 		<Router>
+			{/* 默认首页 */}
 			<Route
-				component={Home}
+				component={pages[0].component}
 				exact
 				path="/"
 			/>
-			<Route
-				component={Home}
-				path="/home"
-			/>
+			{pages.map((item)=>{
+				return (
+					<Route
+						component={item.component}
+						key={item.name}
+						path={item.path}
+					/>
+				)
+			})}
 		</Router>
 	)
 }
---
+++ src/router/pages.js
@@ -0,0 +1,10 @@
+// 页面路由字典配置
+import Home from '@/pages/home'
+
+export default [
+	{
+		component: Home,
+		path: '/home',
+		name: 'Home'
+	}
+]
