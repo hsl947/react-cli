12f6923d9ae1bf5192bbb799df5f1b4938fffbdf
hsl947 <1506070803@qq.com>
2020-02-09 11:54:40 +0800

    引入less

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,23 @@
 //用于修改webpack默认配置
 const path = require('path')
+const { override, fixBabelImports, addLessLoader, addWebpackAlias } = require('customize-cra')
 
-module.exports = function override(config) {
-	// do stuff with the webpack config...
-	// 设置别名路径
-	config.resolve.alias = {
-		...config.resolve.alias,
-		'@': path.resolve(__dirname, './src')
-	}
-	return config
-}
+module.exports = override(
+	// 针对antd 实现按需打包：根据import来打包 (使用babel-plugin-import)
+	fixBabelImports('import',{
+		libraryName:'antd',
+		libraryDirectory:'es',
+		style:true//自动打包相关的样式 默认为 style:'css'
+	}),
+
+	// 使用less-loader对源码重的less的变量进行重新制定，设置antd自定义主题
+	addLessLoader({
+		javascriptEnabled: true,
+		modifyVars:{'@primary-color':'#1DA57A'}
+	}),
+
+	 // 配置路径别名
+	 addWebpackAlias({
+		'@': path.resolve('src')
+	})
+)
